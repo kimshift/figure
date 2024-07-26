18be18a653b44cc0c0b3aa94029ca7871ac5cc9f
chenzhifeng <chenzhifeng@ly-sky.com>
2024-07-26 11:10:13 +0800

    update figure

--- shell/publish.sh
+++ shell/publish.sh
@@ -1,14 +1,14 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-git pull origin master
+git pull gitee master
 git pull github master
 
 git add -A
 git commit -m 'update figure'
 
 # # 上传到gitee仓库
-git push origin master
+git push gitee master
 echo "已同步到gitee仓库"
 
 # # 上传到github仓库
